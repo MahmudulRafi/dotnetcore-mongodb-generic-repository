c039bd93230c95a29b214c7fed7ff09f94dc23d2
Mahmudul-Rafi <mahmudul.rafi@selise.ch>
2023-07-11 11:42:08 +0600

    modified to abstract class

--- DemoAPI.Core/Entities/BaseEntity.cs
+++ DemoAPI.Core/Entities/BaseEntity.cs
@@ -3,17 +3,18 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace DemoAPI.Core.Entities
 {
-    public class BaseEntity
+    public abstract class BaseEntity
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
         [BsonElement("added_by")]
         public string? AddedBy { get; set; }
+        [BsonElement("updated_by")]
+        public string? UpdatedBy { get; set; }
         [BsonElement("created_date")]
         public DateTime? CreatedDate { get; set; }
         [BsonElement("updated_date")]
         public DateTime? UpdatedDate { get; set; }
-
     }
 }
